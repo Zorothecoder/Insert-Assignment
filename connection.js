987b60279d8ae682a07f16ba2d67f9eb6a07e8c3
Basab  & Chewang <141945518+Zorothecoder@users.noreply.github.com>
2023-09-11 18:23:35 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 
 var con = mysql.createConnection({
-    host: 'localhost', 
-    user: 'root', 
-    password: '', 
-    database: 'internshala'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_NAME
 });
 
 con.connect(function(error){
